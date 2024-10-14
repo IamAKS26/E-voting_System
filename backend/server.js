9abc35546dca5ffd2e073073585d3be0b2527f81
sh66n <shaanedroos85@gmail.com>
2024-10-14 21:10:14 +0530

    connected frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const db = require('./db');
-require('dotenv').config();
+const db = require("./db");
+require("dotenv").config();
 
-const bodyParser = require('body-parser'); 
+const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
-const cors = require("cors")
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 // Import the router files
-const userRoutes = require('./routes/userRoutes');
-const candidateRoutes = require('./routes/candidateRoutes');
+const userRoutes = require("./routes/userRoutes");
+const candidateRoutes = require("./routes/candidateRoutes");
 
 // Use the routers
-app.use('/user', userRoutes);
-app.use('/candidate', candidateRoutes);
-app.use(cors());
+app.use(cors(corsOptions));
+app.use("/user", userRoutes);
+app.use("/candidate", candidateRoutes);
 
-
-app.listen(PORT, ()=>{
-    console.log('listening on port 3000');
-})
+app.listen(PORT, () => {
+  console.log("listening on port 3000");
+});
